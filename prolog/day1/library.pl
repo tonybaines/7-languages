fa075fd0dc60b20206e094f5d11cc61803f45d66
Tony Baines <tony.baines.1@gmail.com>
2011-10-28 19:08:56 +0100

    Prolog day1

---
+++ prolog/day1/library.pl
@@ -0,0 +1,36 @@
+author('Ian M. Banks').
+author('William Gibson').
+author('Neil Stephenson').
+author('Stephen Donaldson').
+author('Terry Pratchet').
+
+
+wrote('Excession', 'Ian M. Banks').
+wrote('Neuromancer', 'William Gibson').
+wrote('Snow_crash', 'Neil Stephenson').
+wrote('Chronicles of Thomas Covenant', 'Stephen Donaldson').
+wrote('Count Zero', 'William Gibson').
+wrote('Player Of Games', 'Ian M. Banks').
+wrote('Thief Of Time', 'Terry Pratchet').
+
+written_by(Author, Book) :- wrote(Book, Author).
+
+/* 
+?- author(Who).
+Who = 'Ian M. Banks' ;
+Who = 'William Gibson' ;
+Who = 'Neil Stephenson' ;
+Who = 'Stephen Donaldson' ;
+Who = 'Terry Pratchet'.
+
+?- wrote(What, 'Ian M. Banks').
+What = 'Excession' ;
+What = 'Player Of Games' ;
+false.
+
+?- written_by('Stephen Donaldson', What).
+What = 'Chronicles of Thomas Covenant' ;
+false.
+
+
+*/
---
+++ prolog/day1/musicians.pl
@@ -0,0 +1,47 @@
+plays(fred, guitar). 
+plays(barney, guitar). plays(barney, piano).
+plays(wilma, flute). 
+plays(betty, piano). plays(betty, flute).
+plays(bam_bam, bass_drum).
+plays(dino, piano).
+
+enjoys(blues, dino). enjoys(blues, barney). enjoys(blues, wilma).
+enjoys(jazz, wilma). enjoys(jazz, fred). 
+enjoys(rock, bam_bam). 
+enjoys(classical, dino). enjoys(classical, betty). enjoys(classical, barney). enjoys(classical, wilma).
+
+piece(Genre, Instrument, Who) :- plays(Who, Instrument), enjoys(Genre, Who).
+
+/*
+?- plays(Who, guitar).
+Who = fred ;
+Who = barney ;
+false.
+
+?- piece(classical, piano, Who).
+Who = barney ;
+Who = betty ;
+Who = dino ;
+false.
+
+?- piece(classical, Instrument, betty).
+Instrument = piano ;
+Instrument = flute ;
+false.
+
+?- piece(classical, Instrument, Who).
+Instrument = guitar,
+Who = barney ;
+Instrument = piano,
+Who = barney ;
+Instrument = flute,
+Who = wilma ;
+Instrument = piano,
+Who = betty ;
+Instrument = flute,
+Who = betty ;
+Instrument = piano,
+Who = dino ;
+false.
+
+*/
