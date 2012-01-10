695bfd1b9a39da065f67238779d381a0dc4f113f
802661379 <tony.baines@bt.com>
2012-01-10 14:09:33 +0000

    Prettier io formatting

--- erlang/day1/the_count.erl
+++ erlang/day1/the_count.erl
@@ -1,6 +1,6 @@
 -module(the_count).
 -export([loves_to_count/1]).
 
-loves_to_count(0) -> io:format("");
-loves_to_count(N) -> loves_to_count(N-1), io:format(integer_to_list(N)++"~n"). %% Why is printing a number so *hard* ?
+loves_to_count(0) -> io:format("Ah ha ha!");
+loves_to_count(N) -> loves_to_count(N-1), io:format("~B~n",[N]). %% Why is printing a number so *hard* ?
 
