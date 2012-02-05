f00152793d8b42305a2f7cb23cc11cd3b9b37c53
Tony Baines <tony.baines.1@gmail.com>
2012-02-05 21:53:51 +0000

    Second Clojure exercise

--- clojure/day1/src/day1/core.clj
+++ clojure/day1/src/day1/core.clj
@@ -1,3 +1,10 @@
 (ns day1.core)
 
 (defn big [word size] (> (count word) size))
+
+(defn collection-type [c] 
+(cond
+  (list? c) :list
+  (map? c) :map
+  (vector? c) :vector)
+)
--- clojure/day1/test/day1/test/core.clj
+++ clojure/day1/test/day1/test/core.clj
@@ -10,3 +10,8 @@
   (is (not (big "Four" 5)))
   (is (not (big "Four" 4))))
 
+
+(deftest collections
+  (is (= :list (collection-type (list 1 2 3))))
+  (is (= :map (collection-type {:one 1, :two 2, :three 3})))
+  (is (= :vector (collection-type [1 2 3]))))
